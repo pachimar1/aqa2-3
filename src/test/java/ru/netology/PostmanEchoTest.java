a0f51dea478338f764d5f223ba97b3db355cca40
pachimar1 <mihail.lebedev88@gmail.com>
2023-06-16 15:45:25 +0600

    first commit and all test check

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,42 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void testWithPostmanEcho() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+        ;
+    }
+    @Test
+    void testWithPostmanEchoCyrillicScript() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Добро пожаловать")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Добро пожаловать"));
+        ;
+    }
+}
